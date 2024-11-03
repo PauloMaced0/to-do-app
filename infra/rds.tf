828dc678b6535f861c2ac3703033f06c60e01d22
PauloMaced0 <paulomacedo@ua.pt>
2024-11-03 15:45:35 +0000

    rds bucket without iam policies

---
+++ infra/rds.tf
@@ -0,0 +1,18 @@
+resource "aws_db_subnet_group" "default" {
+  name       = "db-subnet"
+  subnet_ids = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+}
+
+resource "aws_db_instance" "todo_rds" {
+  allocated_storage    = 10
+  db_name              = "todo_db"
+  engine               = "postgres"
+  engine_version       = "16.4"
+  instance_class       = "db.t3.micro"
+  username             = var.db_username
+  password             = var.db_password
+  skip_final_snapshot  = true
+
+  db_subnet_group_name = aws_db_subnet_group.default.name
+  availability_zone = var.availibilty_zone_1
+}
