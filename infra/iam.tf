6f23748df2c93bafb894eb3cd1f179dc223aea36
PauloMaced0 <paulomacedo@ua.pt>
2024-11-03 15:46:15 +0000

    add iam policies

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,7 +1,14 @@
-
-resource "aws_iam_role" "ecsTaskExecutionRole" {
-    name                  = "test-app-ecsTaskExecutionRole"
-    assume_role_policy    = data.aws_iam_policy_document.assume_role_policy.json
+data "aws_iam_policy_document" "policy" {
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogStreams"
+    ]
+    resources = ["*"]
+  }
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
@@ -15,9 +22,9 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-    role                  = aws_iam_role.ecsTaskExecutionRole.name
-    policy_arn            = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+    name                  = "test-app-ecsTaskExecutionRole"
+    assume_role_policy    = data.aws_iam_policy_document.assume_role_policy.json
 }
 
 resource "aws_iam_role" "ecsTaskRole" {
@@ -25,7 +32,24 @@ resource "aws_iam_role" "ecsTaskRole" {
     assume_role_policy    = data.aws_iam_policy_document.assume_role_policy.json   
 }
 
-resource "aws_iam_role_policy_attachment" "ecsTaskRole_policy" {
+resource "aws_iam_policy" "policy" {
+  name        = "ecs-logs-policy"
+  description = "ECS permissions to write on logs"
+  policy      = data.aws_iam_policy_document.policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+    role                  = aws_iam_role.ecsTaskExecutionRole.name
+    policy_arn            = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy_logs" {
+    role                  = aws_iam_role.ecsTaskExecutionRole.name
+    policy_arn            = aws_iam_policy.policy.arn
+}
+
+
+resource "aws_iam_role_policy_attachment" "ecsTaskRole_policy_logs" {
     role                  = aws_iam_role.ecsTaskRole.name
-    policy_arn            = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+    policy_arn            = aws_iam_policy.policy.arn
 }
